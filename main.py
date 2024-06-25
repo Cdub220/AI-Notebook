85df90259e0e3eb6b2e3ee39f8c7778e1c1da2c6
Cdub <Chase.williamson220@gmail.com>
2024-06-24 20:39:37 -0400

    Added subject folder and created page button functionality

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ class Notebook(QMainWindow):
         file_menu_item.addAction(create_subject_action)
         create_subject_action.triggered.connect(self.subject)
         file_menu_item.addAction(create_page_action)
+        create_page_action.triggered.connect(self.page)
         file_menu_item.addAction(save_page_action)
         file_menu_item.addAction(exit_page_action)
 
@@ -43,9 +44,9 @@ class Notebook(QMainWindow):
         subject_label.setStyleSheet("text-align: center; font-size:12pt")
 
         # Create Subject combo box
-        subject_box = QComboBox(self)
-        subject_box.addItems([""])
-        subject_box.setGeometry(70, 60, 200, 40)
+        self.subject_box = QComboBox(self)
+        self.load_subjects()
+        self.subject_box.setGeometry(70, 60, 200, 40)
 
         # Create Note Input
         note_area = QTextEdit(self)
@@ -59,6 +60,56 @@ class Notebook(QMainWindow):
     def subject(self):
         dialog = CreateSubject()
         dialog.exec()
+
+    def page(self):
+        dialog = CreatePage()
+        dialog.exec()
+
+    def new_subject(self):
+        with open("subjects/subject.txt", "r") as file:
+            subject = file.read()
+        new_subject = subject.split(sep=",")
+        new_subject = new_subject[-2]
+        self.subject_box.addItem(new_subject)
+
+    def load_subjects(self):
+        with open("subjects/subject.txt", "r") as file:
+            subject = file.read()
+        subject_list = subject.split(sep=",")
+        subject_list = subject_list[:-1]
+        self.subject_box.addItems(subject_list)
+
+
+class CreatePage(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Create Page")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        subject_box = QComboBox(self)
+        with open("subjects/subject.txt", "r") as file:
+            subject = file.read()
+        subject_list = subject.split(sep=",")
+        subject_list = subject_list[:-1]
+        subject_box.addItems(subject_list)
+        layout.addWidget(subject_box)
+
+        self.page = QLineEdit(self)
+        self.page.setPlaceholderText("Page Title:")
+        layout.addWidget(self.page)
+
+        add_button = QPushButton("Add")
+        add_button.clicked.connect(self.add_page)
+        layout.addWidget(add_button)
+
+        self.setLayout(layout)
+
+    def add_page(self):
+        pass
+
 
 
 class CreateSubject(QDialog):
@@ -84,11 +135,10 @@ class CreateSubject(QDialog):
 
     def add_subject(self):
         subject = self.subject.text()
+        with open("subjects/subject.txt", "a") as file:
+            file.write(f"{subject.title()},")
+        notebook_app.new_subject()
         self.close()
-        return subject
-
-
-
 
 
 app = QApplication(sys.argv)
